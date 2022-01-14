7ce2261b508fc3cac6695e5b0369555d2df801de
antkeyes <keyes.anthony@gmail.com>
2022-01-14 17:53:23 -0500

    added structures and print function

--- race-simulator.c
+++ race-simulator.c
@@ -3,12 +3,29 @@
 #include <time.h>
 
 // Structures section
+struct Race {
+    int numberOfLaps;
+    int currentLap;
+    char* firstPlaceDriverName;
+    char* firstPlaceRaceCarColor;
+};
+
+struct raceCar {
+    char* driverName;
+    char* raceCarColor;
+    int totalLapTime;
+};
 
 // Print functions section
+const char* printIntro(void) {
+    return "Welcome to the race\n\n";
+}
 
 // Logic functions section
 
 int main() {
 	srand(time(0));
+
+    printf("%s", printIntro());
   
 };
