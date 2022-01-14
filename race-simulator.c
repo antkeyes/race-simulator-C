5e0c80226b9729fcd48b6c0028944a50df55b5ee
antkeyes <keyes.anthony@gmail.com>
2022-01-14 18:04:42 -0500

    added first place lap printing function

--- race-simulator.c
+++ race-simulator.c
@@ -18,7 +18,16 @@ struct raceCar {
 
 // Print functions section
 const char* printIntro(void) {
-    return "Welcome to the race\n\n";
+    return "\n\nWelcome to the race\n\n";
+}
+
+const char* printCountDown(void) {
+    return "Races ready! In...\n5\n4\n3\n2\n1\nRace!\n\n";
+}
+
+void printFirstPlaceAfterLap(struct Race race) {
+    printf("After lap number %d\n", race.currentLap); 
+    printf("First place is: %s in the %s car!\n\n", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
 }
 
 // Logic functions section
@@ -27,5 +36,9 @@ int main() {
 	srand(time(0));
 
     printf("%s", printIntro());
+    printf("%s", printCountDown());
+
+    struct Race race1 = {4, 2, "Ant", "green"};
+    printFirstPlaceAfterLap(race1);
   
 };
