453ca0cf4935e56c9b6bebc4dfb0a5db8489c169
antkeyes <keyes.anthony@gmail.com>
2022-01-14 15:41:12 -0500

    setting up project

---
+++ race-simulator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Structures section
+
+// Print functions section
+
+// Logic functions section
+
+int main() {
+	srand(time(0));
+  
+};
